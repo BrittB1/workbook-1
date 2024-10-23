8a8e1387b5ca865f21bd67e40d48af6e5e00bde2
Brittany Buchanan <bbuchanan0400@gmail.com>
2024-10-22 21:09:46 -0400

    Completed simple exercise from Workbook 1-c

---
+++ practice/ShoppingList/src/main/java/com/pluralsight/GroceryList.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    public static void main(String[] args) {
+
+        ArrayList<String> groceries = new ArrayList<String>();
+        groceries.add("Greek Yogurt");
+        groceries.add("Blackberries");
+        groceries.add("Almond Milk");
+        groceries.add("Goat Cheese");
+        groceries.add("Salmon");
+        groceries.add("Spinach");
+        groceries.add("Beets");
+        groceries.add("Wheat Flour");
+        groceries.add("Pumpkin");
+
+        System.out.println(groceries);
+
+
+
+    }
+}
