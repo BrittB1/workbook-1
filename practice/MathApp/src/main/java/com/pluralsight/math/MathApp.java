f1971f5a48fac9d22fbaa0b03c0c754ee043544d
Brittany Buchanan <bbuchanan0400@gmail.com>
2024-10-25 11:38:24 -0400

    Completed Question 2 of Exercise 2 in Workbook 1-Intro to Java

--- practice/MathApp/src/main/java/com/pluralsight/math/MathApp.java
+++ practice/MathApp/src/main/java/com/pluralsight/math/MathApp.java
@@ -3,8 +3,12 @@ package com.pluralsight.math;
 import java.text.NumberFormat;
 import java.util.Locale;
 
+
+
 public class MathApp {
     public static void main(String[] args) {
+
+        System.out.println("QUESTION 1: ");
 
         double bobSalary = 100000;
         double garySalary = 250000;
@@ -15,6 +19,24 @@ public class MathApp {
         String formattedHighestSalary = currencyFormatter.format(highestSalary);
 
        System.out.println("The highest salary is: "+ formattedHighestSalary);
+
+       System.out.println("QUESTION 2: ");
+
+        double truckPrice = 80000;
+        double carPrice = 45000;
+
+        double smallestPrice = Math.min(carPrice,truckPrice);
+
+        String formattedSmallestPrice = currencyFormatter.format(smallestPrice);
+
+        System.out.println("The lowest price is: "+ formattedSmallestPrice);
+
+
+
+
+        //Question 3:
+
+        //Question 4:
 
     }
 }
