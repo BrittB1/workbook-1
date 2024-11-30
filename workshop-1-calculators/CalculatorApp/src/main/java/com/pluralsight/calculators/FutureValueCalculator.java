9cf38d4945f3aef46642f24a2c54ed7863e9fd41
Brittany Buchanan <bbuchanan0400@gmail.com>
2024-11-30 13:29:43 -0500

    Fixed switch statement in CalculatorApp class so case "M" correctly creates a mortgage calculator object + calls constructor instantiated in class MortgageCalculator.

---
+++ workshop-1-calculators/CalculatorApp/src/main/java/com/pluralsight/calculators/FutureValueCalculator.java
@@ -0,0 +1,7 @@
+package com.pluralsight.calculators;
+
+public class FutureValueCalculator {
+
+    private void calculateFutureValue() {
+    }
+}
---
+++ workshop-1-calculators/CalculatorApp/src/main/java/com/pluralsight/calculators/MortgageCalculator.java
@@ -0,0 +1,22 @@
+package com.pluralsight.calculators;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+
+
+public class MortgageCalculator {
+    private Scanner userInput;
+    private NumberFormat currencyFormatter;
+
+
+    public MortgageCalculator() {
+
+        userInput = new Scanner(System.in);
+        currencyFormatter = NumberFormat.getCurrencyInstance();
+    }
+     public void  calculate() {
+
+        //System.out
+     }
+}
