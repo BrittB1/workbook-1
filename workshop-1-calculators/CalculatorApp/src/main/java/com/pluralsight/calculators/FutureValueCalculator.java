816c9ab7eee84821f95f528ef0d9550c2fbaced0
Brittany Buchanan <bbuchanan0400@gmail.com>
2024-12-03 17:09:11 -0500

    Added explanation comments for calculations in 'calculate' method

--- workshop-1-calculators/CalculatorApp/src/main/java/com/pluralsight/calculators/FutureValueCalculator.java
+++ workshop-1-calculators/CalculatorApp/src/main/java/com/pluralsight/calculators/FutureValueCalculator.java
@@ -31,13 +31,17 @@ public class FutureValueCalculator {
 
         /* Future Value Calculation Formula : FV = PV (1+i) ^ n */
 
+        // n calculates # of compounds per year
         double n = 12 * years;
 
+        // i calculates annualRate divided by 100 divided by 12 months per year
         double i = (annualRate / 100) / 12;
 
+        // pow calculates the base raised to the power of n
         double base = 1 + i;
         double pow = Math.pow(base, n);
 
+        // future value calculates principal amount * pow
         double futureValue = principal * pow;
 
         System.out.printf("The future value will be: $%.2f\n", futureValue);
